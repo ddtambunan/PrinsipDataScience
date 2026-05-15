ae914d4e9f28fc96e9f0233bcf5f1f1396ed5d67
Dina Diana Tambunan <63787934+ddtambunan@users.noreply.github.com>
2026-05-15 16:55:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
+from streamlit_jupyter import StreamlitPatcher
 
-# code streamlit kamu
+StreamlitPatcher().jupyter()
+
 st.title("Dashboard Saya")
-st.write("Hello world")
+st.write("Hello from notebook")
